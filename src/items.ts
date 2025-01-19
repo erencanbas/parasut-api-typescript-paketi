74ab5eab68641090f8ba469b6a9d1d68e4f8ffd6
Eren Can Baş <113669882+erencanbas@users.noreply.github.com>
2025-01-19 17:15:57 +0300

    feat: fetching product data and creating new products

---
+++ src/items.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+
+export async function getItems({
+    accessToken,
+    companyId,
+}: {
+    accessToken: string,
+    companyId: string
+}) {
+    const url = `https://api.parasut.com/v4/${companyId}/items`;
+    const headers = {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+    };
+
+    const response = await axios.get(url, { headers });
+    return response.data;
+}
+
+export async function createItem({
+    accessToken,
+    companyId,
+    itemData
+}: {
+    accessToken: string;
+    companyId: string;
+    itemData: {
+        name: string;
+        description: string;
+        price: number;
+    };
+}) {
+    const url = `https://api.parasut.com/v4/${companyId}/items`;
+    const headers = {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+    };
+    const response = await axios.post(url, itemData, {headers});
+    return response.data;
+}
